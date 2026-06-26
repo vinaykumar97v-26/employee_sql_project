da92810ab6d2b6adc55f6be5486b765b81c3a9df
vinaykumar97v-26 <vinaykumar97.v@gmail.com>
2026-06-26 11:35:56 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Employees (Name, Department, Salary, City)
+VALUES
+('Alice','HR',45000,'Chennai'),
+('Bob','IT',65000,'Bangalore'),
+('Charlie','Finance',55000,'Hyderabad'),
+('David','IT',70000,'Chennai'),
+('Eva','Marketing',50000,'Mumbai'),
+('Frank','HR',48000,'Delhi');
